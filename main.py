f0cc616571395e3ae5ec317abc9eed99e768de81
Exr0n <mail@exr0n.com>
2021-08-02 12:31:43 -0700

    minimizing loss doesn't seem to help with accuracy

--- main.py
+++ main.py
@@ -10,19 +10,23 @@ import wandb
 import collections
 from functools import partial
 
-NUM_CLIENTS = 10        # number of clients to sample on each round
+NUM_CLIENTS = 30        # number of clients to sample on each round
 NUM_EPOCHS = 2          # number of times to train for each selected client subset
-NUM_MEGAPOCHS = 20      # number of times to reselect clients
+NUM_MEGAPOCHS = 2000    # number of times to reselect clients
 BATCH_SIZE = 20
 SHUFFLE_BUFFER = 100
 PREFETCH_BUFFER = 10
+
+IMG_WIDTH = 84
+IMG_HEIGHT = 84
 
 def preprocess(dataset):
     def batch_format_fn(element):
         """Flatten a batch `pixels` and return the features as an `OrderedDict`."""
         return collections.OrderedDict(
-            x=tf.reshape(element['image'], [1, 21168]),
-            y=tf.reshape(int(element['attractive']), [-1, 1]))
+            # x=tf.reshape(element['image'], [1, 21168]),
+            x = element['image'],
+            y = tf.reshape(int(element['attractive']), [-1, 1]))
 
     return dataset.repeat(NUM_EPOCHS).shuffle(SHUFFLE_BUFFER).batch(
         BATCH_SIZE).map(batch_format_fn).prefetch(PREFETCH_BUFFER)
@@ -32,10 +36,26 @@ def make_federated_data(data, ids):
 
 def model_factory(spec):
     model = tf.keras.models.Sequential([
-        tf.keras.layers.InputLayer(input_shape=(21168,)),
-        tf.keras.layers.Dense(1000, kernel_initializer='zeros'),
-        tf.keras.layers.Dense(2, kernel_initializer='zeros'),
-        tf.keras.layers.Softmax(),
+        tf.keras.layers.Conv2D(18, (21, 21), input_shape=(IMG_WIDTH, IMG_HEIGHT, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.BatchNormalization(momentum=0.1, epsilon=1e-5),
+
+        tf.keras.layers.Conv2D(32, (6, 6), activation='relu'),
+        tf.keras.layers.BatchNormalization(momentum=0.1, epsilon=1e-5),
+        tf.keras.layers.Dropout(0.1),
+
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(2)
+
+        # tf.keras.layers.Conv2D(32, (21, 21), input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        # tf.keras.layers.MaxPooling2D((2, 2)),
+        # tf.keras.layers.BatchNormalization(momentum=0.1, epsilon=1e-5),
+
+        # tf.keras.layers.InputLayer(input_shape=(21168,)),
+        # tf.keras.layers.Dense(1000, kernel_initializer='zeros'),
+        # tf.keras.layers.Dense(2, kernel_initializer='zeros'),
+        # tf.keras.layers.Softmax(),
     ])
 
     return tff.learning.from_keras_model(
@@ -54,7 +74,7 @@ if __name__ == '__main__':
     # # print(len(celeba_train.client_ids), "\n".join([x for x in celeba_train.element_type_structure]))
     #
     dataset = celeba_train.create_tf_dataset_for_client(celeba_train.client_ids[0])
-    print(dataset.element_spec)
+    # print(dataset.element_spec)
 
     # figure = plt.figure(figsize=(20, 4))
     # j = 0
@@ -68,14 +88,14 @@ if __name__ == '__main__':
     dataset = preprocess(dataset)
     batch = tf.nest.map_structure(lambda x: x.numpy(), next(iter(dataset)))
     #
-    print(list(batch.items())[0][1].shape)
+    # print(list(batch.items())[0][1].shape)
 
-    exit()
+    wandb.init(project="federated-celeba-vanilla", entity="exr0nprojects")
 
     iterative_process = tff.learning.build_federated_averaging_process(
         partial(model_factory, dataset_spec),
-        client_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.02),
-        server_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=1.0))
+        client_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.002),
+        server_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.001))
 
     state = iterative_process.initialize()
 
@@ -85,7 +105,4 @@ if __name__ == '__main__':
     for round_num in range(0, NUM_MEGAPOCHS):
         state, metrics = iterative_process.next(state, dataset)
         print('round {:2d}, metrics={}'.format(round_num+1, metrics))
-
-        exit()
-
-        wandb.log(metrics)
+        wandb.log(metrics['train'])
